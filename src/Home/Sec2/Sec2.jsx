8667fc8585d5dbc4871351be1fe6557361ef5bbf
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-16 18:02:54 +0200

    card *3    Blocchi

--- src/Home/Sec2/Sec2.jsx
+++ src/Home/Sec2/Sec2.jsx
@@ -4,7 +4,9 @@ import './sec2.css';
 const Sec2 = () => {
   return (
     <section className='containerSec2'>
-
+        <div className="cardSec2"></div>
+        <div className="cardSec2"></div>
+        <div className="cardSec2"></div>
     </section>
   )
 }

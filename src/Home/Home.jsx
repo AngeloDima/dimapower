a2787772af0f0ce33d9d24acf414d4212e6f94b2
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-16 23:31:56 +0200

    Sec3 Mega blocchi

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,6 +3,9 @@ import Header from './Header/Header'
 
 import Sec1 from './Sec1/Sec1'
 import Sec2 from './Sec2/Sec2'
+import Sec3 from './Sec3/Sec3'
+
+
 
 const Home = () => {
   return (
@@ -10,6 +13,7 @@ const Home = () => {
         <Header />
         <Sec1 />
         <Sec2 />
+        <Sec3 />
     </div>
   )
 }
---
+++ src/Home/Sec3/Sec3.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './sec3.css';
+
+
+
+const Sec3 = () => {
+  return (
+    <section className='containerSec3'>
+        <div className="contentSec3">
+            <div className="SxSec3"></div>
+            <div className="DxSec3"></div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Sec3

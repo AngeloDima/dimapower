5ef29d43f2ed081eb41b681c3e4978563e0dae8d
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-16 15:25:43 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home/Home';
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />   
+        </Routes>
+       </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

cbfa23500091586ad4264801485eb6fc5fb5b3df
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-16 17:56:26 +0200

    add sec2

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Header/Header'
 
 import Sec1 from './Sec1/Sec1'
+import Sec2 from './Sec2/Sec2'
 
 const Home = () => {
   return (
     <div>
         <Header />
         <Sec1 />
+        <Sec2 />
     </div>
   )
 }
---
+++ src/Home/Sec2/Sec2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './sec2.css';
+
+const Sec2 = () => {
+  return (
+    <section className='containerSec2'>
+
+    </section>
+  )
+}
+
+export default Sec2

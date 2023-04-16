fbcbaf4872a12b7f2846cd37d2ffd538640da7fe
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-16 19:23:24 +0200

    break (card)

--- src/Home/Sec2/Sec2.jsx
+++ src/Home/Sec2/Sec2.jsx
@@ -1,12 +1,67 @@
 import React from 'react'
 import './sec2.css';
 
+
+
+
+
 const Sec2 = () => {
   return (
     <section className='containerSec2'>
-        <div className="cardSec2"></div>
-        <div className="cardSec2"></div>
-        <div className="cardSec2"></div>
+
+
+
+
+
+        <div className="cardSec2">
+
+            <div className="SxSec2">
+                <div className="ballSec2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                </div>
+            </div>
+
+            <div className="DxSec2">
+                <h4>Full - Stack</h4>
+            </div>
+
+        </div>
+
+
+
+
+
+        <div className="cardSec2">
+
+            <div className="SxSec2">
+                <div className="ballSec2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                </div>
+            </div>
+
+            <div className="DxSec2">
+                <h4>Front - End</h4>
+            </div>
+
+        </div>
+
+
+
+
+        <div className="cardSec2">
+
+            <div className="SxSec2">
+                <div className="ballSec2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                </div>
+            </div>
+
+            <div className="DxSec2">
+                <h4>Back - End</h4>
+            </div>
+
+        </div>
+
     </section>
   )
 }

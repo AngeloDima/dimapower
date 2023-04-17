ba1ce247e3074adbf6585a6cad34accfa8cafd10
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-17 02:41:57 +0200

    sec3 finita, (si parte con il responsive)

--- src/Home/Sec3/Sec3.jsx
+++ src/Home/Sec3/Sec3.jsx
@@ -7,12 +7,41 @@ const Sec3 = () => {
   return (
     <section className='containerSec3'>
         <div className="contentSec3">
-            <div className="SxSec3"></div>
-            <div className="DxSec3"></div>
+            <div className="SxSec3">
+                <div className="foto"></div>
+            </div>
+            <div className="DxSec3">
+              <div className="contentDx3">
+                <h3>Hai Bisogno Di un Sito Creativo?</h3>
+                <h3>Posso Aiutarti!</h3>
+                <p>Un web developer è il mago della tecnologia che trasforma le idee in realtà. Attraverso la sua conoscenza di linguaggi di programmazione come HTML, CSS e JavaScript, un web developer crea siti web funzionali, sicuri e accattivanti. Con la sua capacità di gestire database, progettare interfacce utente intuitive e ottimizzare le prestazioni del sito web, un web developer garantisce un'esperienza utente coinvolgente e soddisfacente.</p>
+                
+                <div className="social">
+                  <div className="SocialIcon">
+                    <span className='insta' ></span>
+                    <h5>Angelo_dimauro_</h5>
+                  </div>
 
+                  <div className="SocialIcon">
+                    <span className='Linkedin' ></span>
+                    <h5>Angelo Di Mauro</h5>
+                  </div>
+
+                  <div className="SocialIcon">
+                    <span className='Git' ></span>
+                    <h5>AngeloDima</h5>
+                  </div>
+                </div>
+
+
+
+              </div>
+            </div>
         </div>
     </section>
   )
 }
+
+
 
 export default Sec3

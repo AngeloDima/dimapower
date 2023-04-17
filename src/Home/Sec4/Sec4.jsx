4bb23aac5601fd3d77f51334b162276bb438ad02
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-17 21:15:14 +0200

    building del design

--- src/Home/Sec4/Sec4.jsx
+++ src/Home/Sec4/Sec4.jsx
@@ -10,11 +10,29 @@ const Sec4 = () => {
 
 
             <div className="SxSec4">
+                <h2>Quali linguaggi di <span id="giallo"> programmazione </span> uso?</h2>
+                <p>Come Web Developer, utilizzo una serie di strumenti e tecnologie per creare e gestire siti web e applicazioni web. Questi strumenti includono linguaggi di markup come HTML e CSS, linguaggi di programmazione come JavaScript e PHP, sistemi di gestione di database come MySQL, e framework come React JS, Vue.js e Laravel.</p>
 
+                <div className="containerLinee">
+                    <div className='LineaYellow'></div>
+                    <div className='LineaYellow'></div>
+                    <div className='LineaYellow'></div>
+                </div>
             </div>
 
             <div className="DxSec4">
-                
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
+                <div className="cardSec4"></div>
             </div>
         </div>
     </section>

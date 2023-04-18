aec446c82b933dd0c3f50e7886d4d44e53edf3be
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-18 02:26:16 +0200

    fix project css

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './blog.css';
 
 import Header from '../Home/Header/Header';
+import Sec1 from './Sec1/Sec1';
 
 
 const Blog = () => {
   return (
     <div>
       <Header />
+      <Sec1 />
     </div>
   )
 }
---
+++ src/Blog/Sec1/Sec1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './sec1.css';
+
+const Sec1 = () => {
+  return (
+    <section className='ContainerSec1Blog'>
+        <div className="contentSec1Blog"></div>
+    </section>
+  )
+}
+
+export default Sec1

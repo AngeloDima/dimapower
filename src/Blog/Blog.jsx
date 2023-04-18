a3c8324131ad7768680122595d0370e18f181f87
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-18 03:17:17 +0200

    mega blocchi per il blog

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -3,13 +3,14 @@ import './blog.css';
 
 import Header from '../Home/Header/Header';
 import Sec1 from './Sec1/Sec1';
-
+import Sec2 from './Sec2/Sec2';
 
 const Blog = () => {
   return (
     <div>
       <Header />
       <Sec1 />
+      <Sec2 />
     </div>
   )
 }
---
+++ src/Blog/Sec2/Sec2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './sec2.css';
+
+
+const Sec2 = () => {
+  return (
+    <section className='sec2Blog'>
+        <div className="contentSec2Blog"></div>
+    </section>
+  )
+}
+
+export default Sec2
